641003976ae5ec5c1aa8b40972e69fb92db992d9
Awadesh <36awadesh@gmail.com>
2024-02-04 14:35:55 +0530

    Action Begin For Creation of Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
